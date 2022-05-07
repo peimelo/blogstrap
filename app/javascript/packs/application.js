80a90d6f8880e7db9b847db674d843348a8d23f0
Paulo Melo <peimelo@gmail.com>
2022-05-06 21:09:12 -0300

    Adiciona JavaScript do Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,23 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require('@popperjs/core');
 
 import * as ActiveStorage from '@rails/activestorage';
 import Rails from '@rails/ujs';
-import 'bootstrap';
+import { Tooltip } from 'bootstrap';
 import 'channels';
 import Turbolinks from 'turbolinks';
 import '../stylesheets/application';
+
+document.addEventListener('turbolinks:load', () => {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new Tooltip(tooltipTriggerEl);
+  });
+});
 
 Rails.start();
 Turbolinks.start();
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+const webpack = require('webpack');
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    Popper: ['popper.js', 'default'], // Not a typo, we're still using popper.js here
+  })
+);
+
+module.exports = environment;
